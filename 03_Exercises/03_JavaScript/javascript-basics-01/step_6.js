e61983adf2ff9b2a22772e41d2be85abfcd73f0b
Maya <maya.atiah.99@outlook.com>
2022-12-07 21:28:51 +0200

    Editing step-6

--- 03_Exercises/03_JavaScript/javascript-basics-01/step_6.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_6.js
@@ -1,4 +1,6 @@
 var numberOne=prompt("Type the first number");
+document.getElementById("first_number").value=numberOne;
 var numberTwo=prompt("Type the second number");
+document.getElementById("second_number").value=numberTwo;
 alert("the remainder of "+numberOne+" and "+numberTwo+" is "+numberOne/numberTwo);
 
