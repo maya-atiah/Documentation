a21f6aca786fe1b35e77bd6f6d3e8e5f92acef39
Maya <maya.atiah.99@outlook.com>
2022-12-07 22:05:27 +0200

    Finishing step-7

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_7.js
@@ -0,0 +1,21 @@
+
+
+
+function someCalculations(){
+    var shoeNumber=prompt("Write your shoe number");
+document.getElementById("shoe_size").value=shoeNumber;
+var birthYear=prompt("Write your birth year");
+document.getElementById("year").value=birthYear;
+ alert(shoeNumber*2+5-birthYear+1766);
+    
+}
+someCalculations();
+
+
+// Multiplies the shoe size by 2
+// Adds 5
+// Multiplies by 50
+// Subtracts the birth year
+// Adds 1766
+// Shows the result to the user
+// Commit
