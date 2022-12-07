6fce4c22871c61d81cad1c8a52b355c94c53cff0
Maya <maya.atiah.99@outlook.com>
2022-12-07 20:45:29 +0200

    Finishing step_6

--- 03_Exercises/03_JavaScript/javascript-basics-01/step_5.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_5.js
@@ -1,4 +1,3 @@
 var numberOne=prompt("type the first number");
 var numberTwo=prompt("type the second number");
-
 alert(numberOne*numberTwo);
---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_6.js
@@ -0,0 +1,4 @@
+var numberOne=prompt("Type the first number");
+var numberTwo=prompt("Type the second number");
+alert("the remainder of "+numberOne+" and "+numberTwo+" is "+numberOne/numberTwo);
+
