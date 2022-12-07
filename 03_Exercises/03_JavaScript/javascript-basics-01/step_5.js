7d6be769fa48476a48d5f75742927769d2f6de9c
Maya <maya.atiah.99@outlook.com>
2022-12-07 21:23:49 +0200

    Editing step-5

--- 03_Exercises/03_JavaScript/javascript-basics-01/step_5.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_5.js
@@ -1,3 +1,5 @@
 var numberOne=prompt("type the first number");
+document.getElementById("first_number").value=numberOne;
 var numberTwo=prompt("type the second number");
+document.getElementById("second_number").value=numberTwo;
 alert(numberOne*numberTwo);
