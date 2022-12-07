c65bd9148f2e843d678184c4df93bb2c3f958c89
Maya <maya.atiah.99@outlook.com>
2022-12-07 19:33:30 +0200

    Finishing step-4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_4.js
@@ -0,0 +1,6 @@
+name=prompt("Please enter your name");
+surname=prompt("please enter your surname");
+city=prompt("Please enter your city")
+
+
+alert("Name: "+name+"\nSurname:"+surname+"\ncity:"+city);
