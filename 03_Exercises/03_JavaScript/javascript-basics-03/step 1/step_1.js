697fe43c8d82437e2b88592b3f272dbab8c6f170
Maya <maya.atiah.99@outlook.com>
2022-12-08 20:35:13 +0200

    step-1 done

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 1/step_1.js
@@ -0,0 +1,15 @@
+var img=document.querySelector("#image1")
+
+
+function setBorder(){
+    img.style.border="3px solid red";
+
+
+}
+
+
+function removeBorder(){
+    img.style.border="none";
+
+
+}
