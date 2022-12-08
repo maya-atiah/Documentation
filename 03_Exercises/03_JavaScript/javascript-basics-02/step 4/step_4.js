92f5979a1f273d8c04a16adab5228a98639125e4
Maya <maya.atiah.99@outlook.com>
2022-12-08 16:22:23 +0200

    Finishing step-5 basic2

--- 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step_4.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step_4.js
@@ -1,5 +1,5 @@
-document.querySelector("#btn").addEventListener("click", function () {
-    if (confirm("Are you sure u want to confirm?")) {
+document.querySelector("#buttn").addEventListener("click", function () {
+    if (confirm("Are you sure u want to reset?")) {
       document.querySelector("#name").value = "";
       document.querySelector("#city").value = "";
       document.querySelector("#surname").value = "";
--- 03_Exercises/03_JavaScript/javascript-basics-02/step 5/step_5.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 5/step_5.js
@@ -1,12 +1,7 @@
-var image;
-image=document.querySelector("#image1");
-image.addEventListener("mouseover",myFunction())
-
-
-
-function myFunction(){
-document.getElementById("demo").addEventListener("mouseover", );
-document.getElementById("demo").addEventListener("mouseout", mouseOut);
-document.getElementById("demo").addEventListener("mouseover", mouseOver);
-document.getElementById("demo").addEventListener("mouseover", mouseOver);
-}
+   document.addEventListener("mouseover",function(){
+    document.getElementById("image1").src = "images/image1_2.jpg";
+    document.getElementById("image2").src = "images/image2_2.jpg";
+    document.getElementById("image3").src = "images/image3_2.jbg";
+    document.getElementById("image4").src = "images/image4_2.jpg";
+    document.getElementById("image5").src = "images/image5_2.jpg";
+})
