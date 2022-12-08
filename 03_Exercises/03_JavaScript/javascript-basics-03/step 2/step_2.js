c73033cbebc04005b863e5c522c8669198fd9a8e
Maya <maya.atiah.99@outlook.com>
2022-12-08 21:37:41 +0200

    step 2 done

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/step_2.js
@@ -0,0 +1,35 @@
+
+
+//  document.addEventListener("DOMContentLoaded",myFunction)
+
+
+//  function myFunction(){
+
+//      if(document.getElementById("show").onclick ){
+//  document.querySelector("p").style.visibility="visible";
+// }
+
+//   else  if(document.getElementById("hide").onclick ){
+//      document.querySelector("p").style.visibility="hidden";
+//  }
+
+// }
+// myFunction();
+
+ function showObj()
+{
+    document.querySelector("p").style.display="block";
+}
+
+function hideObj(){
+    document.querySelector("p").style.display="none";
+}
+
+// document.getElementById("show").onclick = function() {
+//     document.getElementById("texte").style.display = "unset";
+// }
+
+// document.getElementById("hide").onclick = function() {
+//     document.getElementById("texte").style.display = "none";
+// }
+
